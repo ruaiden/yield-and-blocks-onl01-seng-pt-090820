931ab9d1cd28f346adff1317fff3749ab5eeb666
Aiden Marques <aiden.marques94@gmail.com>
2020-09-19 17:57:55 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,8 @@
-def hello_t(name)
+def hello_t(names
   i = 0 
 
-while i < name.length 
-  yield name [i]
+while i < names.length
+  yield names[i]
   i = i + 1 
 
 
@@ -11,3 +11,8 @@ end
 
 # call your method here!
 
+hello_t (["Tim","Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
